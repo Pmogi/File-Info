914ef2d9d30156b5a212c7842f825c3508aef372
Patrick Mogianesi <scottishunicorn@gmail.com>
2016-06-13 15:47:22 -0700

    Added comments and spruce up code

--- fileinfo.py
+++ fileinfo.py
@@ -1,35 +1,39 @@
 '''
 Patrick Mogianesi
-
+June 13 2016
 fileinfo script
 
-This program will get the information of a chosen file
-The size of the file
-The Name of file
-
-
-(.stat function)
+This program will get the information of a chosen file:
+The size of the file.
+The Name of file.
+How old the file is.
 '''
 #the user inputs the path for the file
 
+#os.path allows for paths to be understood by the computer
 import os, os.path
-import sys
 
 def main():
 	print "CTRL-C to exit"
+	#input for user
 	path = raw_input("Input path of file > ")
 	
+	#this list takes 3 functions, and finds values for them
+	#basename = name of file
+	#getsize = size of file
+	#getntime = age of file
+	x = [os.path.basename(path), os.path.getsize(path), os.path.getmtime(path)]
 	
-	x = [os.path.basename(path) , os.path.getsize(path), os.path.getmtime(path)]
-	
+	#Simply prints out the info calling for information from the list.
 	print "The files name is %r." % x[0]
 	
 	print "The program is %r bytes big." % x[1]
 
 	print "The file is %r seconds old." % x[2]
 	
+	#sends user back start of the script
 	main()
 	
 
-
+#starts script by calling main function
 main()
